35ad8ce300390859871aaa548048f6dd717610a6
Bryan Finlayson <bryan.finlayson@metova.com>
2018-05-01 16:35:53 -0500

    require files

--- lib/crud_inspector.rb
+++ lib/crud_inspector.rb
@@ -1,5 +1,12 @@
 require 'fileutils'
 
+require 'crud_inspector/files/cleaner'
+require 'crud_inspector/files/manager'
+require 'crud_inspector/files/writer'
+
+require 'crud_inspector/files/mime_types/json'
+require 'crud_inspector/files/mime_types/xml'
+
 module CrudInspector
   class Tracker
     def initialize(options = {})
@@ -10,9 +17,9 @@ module CrudInspector
       return unless ::Rails.env.development?
       case @options[:type]
       when :rest
-        CrudInspector::Files::Manager.new(@options.merge!(type: :rest)).perform
+        ::CrudInspector::Files::Manager.new(@options.merge!(type: :rest)).perform
       when :soap
-        CrudInspector::Files::Manager.new(@options.merge!(type: :soap)).perform
+        ::CrudInspector::Files::Manager.new(@options.merge!(type: :soap)).perform
       else
         "no type given"
       end
